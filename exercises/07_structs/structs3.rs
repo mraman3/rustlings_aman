91f2b31030a54e94cc6e8ca05104388032297544
Aman Braich <braich_aman3@yahoo.ca>
2024-12-26 20:15:45 -0500

    Exercises 07 Completed

--- exercises/07_structs/structs3.rs
+++ exercises/07_structs/structs3.rs
@@ -24,19 +24,29 @@ impl Package {
     }
 
     // TODO: Add the correct return type to the function signature.
-    fn is_international(&self) {
+    fn is_international(&self) -> bool {
         // TODO: Read the tests that use this method to find out when a package
         // is considered international.
+        self.sender_country != self.recipient_country
     }
 
     // TODO: Add the correct return type to the function signature.
-    fn get_fees(&self, cents_per_gram: u32) {
+    fn get_fees(&self, cents_per_gram: u32) -> u32 {
         // TODO: Calculate the package's fees.
+        self.weight_in_grams * cents_per_gram
     }
 }
 
 fn main() {
     // You can optionally experiment here.
+
+    let sender_country = String::from("Spain");
+    let recipient_country = String::from("Russia");
+
+    let package = Package::new(sender_country, recipient_country, 1200);
+
+    println!("{}", package.is_international());
+    println!("{}", package.get_fees(1));
 }
 
 #[cfg(test)]
